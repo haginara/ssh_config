89edd02686ca2b411e642d75c5f7cb900c177f9d
jonghak choi <jhchoi@nexon.net>
2019-03-18 14:20:44 -0700

    Change test_ls

--- tests/ssh_config_test.py
+++ tests/ssh_config_test.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.INFO)
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from ssh_config import SSHConfig, Host, __version__
 from ssh_config import cli
+
 sample = os.path.join(os.path.dirname(__file__), "sample")
 
 new_host = Host("server2", {"ServerAliveInterval": 200, "HostName": "203.0.113.77"})
@@ -89,38 +90,54 @@ else:
     from io import StringIO
 
 from contextlib2 import redirect_stdout
+
+
 class TestSSHCli(unittest.TestCase):
     def test_cli(self):
         try:
             f = StringIO()
             with redirect_stdout(f):
-                cli.main(['ssh_config', '-v'])
+                cli.main(["ssh_config", "-v"])
             output = f.getline().strip()
-            self.assertEqual('ssh_config 0.0.4', output)
+            self.assertEqual("ssh_config 0.0.4", output)
         except SystemExit:
             pass
-    
+
     def test_ls(self):
+        expect = [
+            "server1: 203.0.113.76",
+            "*: None",
+            "server_cmd_1: 203.0.113.76",
+            "server_cmd_2: 203.0.113.76",
+            "server_cmd_3: 203.0.113.76",
+        ]
         try:
             f = StringIO()
             with redirect_stdout(f):
-                cli.main(['ssh_config', '-f', sample, 'ls'])
+                cli.main(["ssh_config", "-f", sample, "ls"])
             output = f.getvalue()
-            result = ("server1: 203.0.113.76\n"
-                    "*: None\n"
-                    "server_cmd_1: 203.0.113.76\n"
-                    "server_cmd_2: 203.0.113.76\n"
-                    "server_cmd_3: 203.0.113.76\n")
-            self.assertEqual(result, output)
+            for line in output.split("\n"):
+                if line:
+                    self.assertIn(line, expect)
         except SystemExit:
             pass
-    
+
     def test_add(self):
         try:
             sample_add = os.path.join(os.path.dirname(__file__), "sample.add")
-            cli.main(['ssh_config', '-f', sample_add, 'add', '-f', 'test_add', 'HostName=238.0.4.1'])
+            cli.main(
+                [
+                    "ssh_config",
+                    "-f",
+                    sample_add,
+                    "add",
+                    "-f",
+                    "test_add",
+                    "HostName=238.0.4.1",
+                ]
+            )
             sshconfig = SSHConfig.load(sample_add)
-            host = sshconfig.get('test_add', raise_exception=False)
+            host = sshconfig.get("test_add", raise_exception=False)
             self.assertIsNotNone(host)
         except SystemExit:
             pass
@@ -128,21 +145,23 @@ class TestSSHCli(unittest.TestCase):
     def test_rm(self):
         try:
             sample_add = os.path.join(os.path.dirname(__file__), "sample.add")
-            cli.main(['ssh_config', '-f', sample_add, 'rm', '-f', 'test_add'])
+            cli.main(["ssh_config", "-f", sample_add, "rm", "-f", "test_add"])
             sshconfig = SSHConfig.load(sample_add)
-            host = sshconfig.get('test_add', raise_exception=False)
+            host = sshconfig.get("test_add", raise_exception=False)
             self.assertIsNone(host)
         except SystemExit:
             pass
-        
+
     def test_import(self):
         try:
             sample_import = os.path.join(os.path.dirname(__file__), "sample.import")
             import_csv = os.path.join(os.path.dirname(__file__), "import.csv")
-            cli.main(['ssh_config', '-f', sample_import, 'import', '-q', '-f', import_csv])
+            cli.main(
+                ["ssh_config", "-f", sample_import, "import", "-q", "-f", import_csv]
+            )
             sshconfig = SSHConfig.load(sample_import)
-            import_1 = sshconfig.remove('import1')
-            import_2 = sshconfig.remove('import2')
+            import_1 = sshconfig.remove("import1")
+            import_2 = sshconfig.remove("import2")
             sshconfig.write()
             self.assertTrue(import_1)
             self.assertTrue(import_2)
