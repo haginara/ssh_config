aa43062bb83c1c61cd0f52d4ef821469550b5b71
jonghak choi <haginara@gmail.com>
2019-03-28 00:13:01 -0700

    Fix logic bug on export

--- ssh_config/__init__.py
+++ ssh_config/__init__.py
@@ -1,5 +1,5 @@
 from .client import SSHConfig, Host, EmptySSHConfig
 
-__version__ = ".".join(map(str, (0, 0, 10)))
+__version__ = ".".join(map(str, (0, 0, 11)))
 
 __all__ = [EmptySSHConfig, SSHConfig, Host]
