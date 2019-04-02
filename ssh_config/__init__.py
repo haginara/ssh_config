a2a45dea29ed06110d5b7ca8aeb7749dde9049f4
jonghak choi <jhchoi@nexon.net>
2019-04-02 10:19:02 -0700

    Version 0.0.12

--- ssh_config/__init__.py
+++ ssh_config/__init__.py
@@ -1,5 +1,5 @@
 from .client import SSHConfig, Host, EmptySSHConfig
 
-__version__ = ".".join(map(str, (0, 0, 11)))
+__version__ = ".".join(map(str, (0, 0, 12)))
 
 __all__ = ["EmptySSHConfig", "SSHConfig", "Host"]
