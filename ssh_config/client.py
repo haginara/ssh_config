5a0b0e33daf2499b34e5bfc771c05b33abfef812
jonghak choi <jhchoi@nexon.net>
2019-09-09 21:50:10 -0700

    Add attributes

--- ssh_config/client.py
+++ ssh_config/client.py
@@ -40,26 +40,47 @@ class WrongSSHConfig(Exception):
 
 class Host(object):
     attrs = [
+        ("AddressFamily", str), # any, inet, inet6
+        ("BatchMode", str),
+        ("BindAddress", str),
+        ("ChallengeResponseAuthentication", str), # yes, no
+        ("CheckHostIP", str), # yes, no
+        ("Cipher", str),
+        ("Ciphers", str),
+        ("ClearAllForwardings", str), # yes, no
+        ("Compression", str), # yes, no
+        ("CompressionLevel", int), # 1 to 9
+        ("ConnectionAttempts", int), # default: 1
+        ("ConnectTimeout", int),
+        ("ControlMaster", str), # yes, no
+        ("ControlPath", str), 
+        ("DynamicForward", str), #[bind_address:]port, [bind_adderss/]port
+        ("EnableSSHKeysign", str), # yes, no
+        ("EscapeChar", str), #default: '~'
+        ("ExitOnForwardFailure", str), #yes, no
+        ("ForwardAgent", str), # yes, no
+        ("ForwardX11", str), # yes, no
+        ("ForwardX11Trusted", str), # yes, no
+        ("GatewayPorts", str), # yes, no
+        ("GlobalKnownHostsFile", str), # yes, no
+        ("GSSAPIAuthentication", str), # yes, no
         ("HostName", str),
         ("User", str),
         ("Port", int),
         ("IdentityFile", str),
-        ("ProxyCommand", str),
         ("LocalCommand", str),
         ("LocalForward", str),
+        ("LogLevel", str),
+        ("ProxyCommand", str),
         ("Match", str),
         ("AddKeysToAgent", str),
-        ("AddressFamily", str),
-        ("BatchMode", str),
-        ("BindAddress", str),
         ("BindInterface", str),
         ("CanonialDomains", str),
         ("CnonicalizeFallbackLocal", str),
         ("IdentityAgent", str),
-        ("LogLevel", str),
         ("PreferredAuthentications", str),
         ("ServerAliveInterval", int),
-        ("ForwardAgent", str),
+        ("UsePrivilegedPort", str), # yes, no
     ]
 
     def __init__(self, name, attrs):
@@ -164,7 +185,7 @@ class SSHConfig(object):
         SEP = Suppress(SPACE) | Suppress("=")
         HOST = CaselessLiteral("Host").suppress()
         KEY = Word(alphanums)
-        VALUE = Word(alphanums + " ~%*?!._-+/,")
+        VALUE = Word(alphanums + ' ~%*?!._-+/,"')
         paramValueDef = SkipTo("#" | lineEnd)
         indentStack = [1]
 
